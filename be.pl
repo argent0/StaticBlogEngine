a97d28b77fda283af2247a0b65fdbe07e8a4da25
Aner Lucero <4rgento@gmail.com>
2012-04-21 13:30:54 -0300

    Adding generation from templates.

--- be.pl
+++ be.pl
@@ -69,4 +69,27 @@ my @posts = Iterator::iterToList(
 say Dumper( sort { DateToNumber($$a{date}) cmp DateToNumber($$b{date}) }
 	@posts );
 
+# some useful options (see below for full list)
+my $config = {
+    INCLUDE_PATH => 'templates',  # or list ref
+    INTERPOLATE  => 1,               # expand "$var" in plain text
+    POST_CHOMP   => 1,               # cleanup whitespace
+	 #PRE_PROCESS  => 'header',        # prefix each template
+    EVAL_PERL    => 1,               # evaluate Perl code blocks
+};
+
+# create Template object
+my $template = Template->new($config);
+
+# define template variables for replacement
+my $vars = {
+	Title => 'This is the title',
+};
+
+# specify input filename, or file handle, text reference, etc.
+my $input = 'post.tt';
+
+# process input template, substituting variables
+$template->process($input, $vars)
+    || die $template->error();
 
